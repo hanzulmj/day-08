026aac0010509f58aa082742dbe05243127b56ed
hanzulmj <139529221+hanzulmj@users.noreply.github.com>
2024-11-26 08:51:37 +0000

    navlink

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -8,13 +8,13 @@ function App() {
   return (
     <div className="App">
        <Router>
-        <nev>
+        <nav>
           <ul>
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
           </ul>
-        </nev>
+        </nav>
         <Routes>
           <Route path='/home' element={<Home/>} />
           <Route path='/about' element={<AboutUs/>} />
