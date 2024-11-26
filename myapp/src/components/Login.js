5cdbfe6b53fe59b7bbd0bec9bad8b4cd674bc718
hanzulmj <139529221+hanzulmj@users.noreply.github.com>
2024-11-26 09:31:36 +0000

    loggin page is updated with the form function

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 //we use useNavigate hook here
 //so we have to use it above the export default function
@@ -7,10 +8,27 @@ export default function Login(){
         navigate('/dashboard');
     }
 
+    const[name,setName]=useState("Hanzul");
+    const[password,setPassword]=useState("ysfmee123");
+
+    if (setName === setPassword) {
+        console.log("You are logged in");
+    } else {
+        console.log("Your password or username is incorrect");
+    }
     return(
         <div>
+            <div>
             <h1>Login Here</h1>
             <button onClick={authenticate}>Login</button>
+            </div>
+        <form>
+            <label>Username</label>
+            <input type="text" placeholder="enter your username" name="username" />
+            <label>Password</label>
+            <input type="password" placeholder="enter your password" name="password" />
+
+        </form>
         </div>
     )
 }
