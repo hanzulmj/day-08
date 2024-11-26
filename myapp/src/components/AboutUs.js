c35c3003c751e615c2d05fbb70918711f5cd7bb4
hanzulmj <139529221+hanzulmj@users.noreply.github.com>
2024-11-26 08:43:46 +0000

    created components home aboutus and contactus

---
+++ myapp/src/components/AboutUs.js
@@ -0,0 +1,7 @@
+export default function AboutUs(){
+    return(
+        <div>
+            <h1>Welcome to About Us page</h1>
+        </div>
+    );
+}
---
+++ myapp/src/components/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs(){
+    return(
+        <div>
+            <h1>Welcome to Contact Us page</h1>
+        </div>
+    );
+}
