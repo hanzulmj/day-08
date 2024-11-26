11874089c1af72084e3963a2cf333cfa83347790
hanzulmj <139529221+hanzulmj@users.noreply.github.com>
2024-11-26 09:15:53 +0000

    cganges in useNavigate

--- myapp/src/components/Login.js
+++ myapp/src/components/Login.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-
+//we use useNavigate hook here
+//so we have to use it above the export default function
 export default function Login(){
     const navigate = useNavigate();
     const authenticate=()=>{
